dfa7465814f7dc58d01341a7776afe4532c82fb0
mirru2532 <kirrukirruheroku5@protonmail.com>
2022-02-24 16:00:29 +0100

    feat: v1.2.0

--- contracts/index.js
+++ contracts/index.js
@@ -2,4 +2,18 @@ const distribution = require("./migrations/distro.json")
 const contracts = require("./contracts.json")
 const rewards = require("./vlcvx_token_rewards.json")
 
-module.exports = { distribution , contracts, rewards }
+const findPool = function (key) {
+    for(const pool of contracts.pools) {
+        for(const element of pool) {
+	    if(element == key) {
+		return pool;
+	    }
+	}
+    }
+}
+
+const utils = { findPool }
+
+const version = "1.2.0"
+
+module.exports = { version, utils, distribution , contracts, rewards }
